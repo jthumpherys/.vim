7515d4ee64226359a455bff1262a969e9623e752
Jade Humpherys <joshua.humpherys@protonmail.com>
2023-04-24 16:39:45 -0500

    remove non-functioning automatic plug installation and unnecessary updating, add lua to treesitter installs for use with nvim scripts

--- init.lua
+++ init.lua
@@ -14,6 +14,7 @@ vim.call('plug#begin', plug_path)
 Plug 'junegunn/vim-plug'
 
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = vim.fn['TSUpdate#']})
+Plug 'nvim-treesitter/playground'
 
 -- fuzzy search ig?
 Plug 'nvim-lua/plenary.nvim'
@@ -50,21 +51,9 @@ vim.call('plug#end')
 vim.cmd("colorscheme carbonfox")
 
 
-vim.cmd('PlugUpdate')
-vim.cmd('q')
-
-for plug in pairs(vim.g.plugs) do
-  local ok, err = os.rename(plug_path .. '/' .. plug, plug_path .. '/' .. plug)
-  if not ok then
-    vim.cmd('PlugInstall')
-    break
-  end
-end
-
-
 -- nvim-treesitter setup
 require("nvim-treesitter.configs").setup {
-  ensure_installed = { "bash", "cpp", "json", "make", "python", "vim" },
+  ensure_installed = { "bash", "cpp", "json", "lua", "make", "python", "vim" },
 
   sync_install = false,
 
