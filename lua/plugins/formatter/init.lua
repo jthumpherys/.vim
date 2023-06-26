f6e4ca4c1627eb46f81d768e47f6b25c0c31f750
Jade Humpherys <joshua.humpherys@protonmail.com>
2023-06-25 23:49:31 -0500

    get rid of defunct files

--- lua/plugins/formatter/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- "mhartington/formatter.nvim",
-  -- name = "formatter",
-  -- opts = require("plugins.formatter.config").options,
-  -- ft = function(_, opts) return opts.filetype end,
-}
