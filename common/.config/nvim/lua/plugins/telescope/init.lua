192813b31aeb380164805bb1d42e03a61ecbb605
Jade Humpherys <joshua.humpherys@protonmail.com>
2023-06-26 22:54:53 -0500

    small formatting fix

--- common/.config/nvim/lua/plugins/telescope/init.lua
+++ common/.config/nvim/lua/plugins/telescope/init.lua
@@ -1,20 +1,22 @@
 local M = {require("plugins.telescope.config").get_plugins()}
 
-table.insert(M, {
-  "nvim-telescope/telescope.nvim",
-  name = "telescope",
-  opts = function()
-    local options = {}
-    local extensions = require("plugins.telescope.config").extensions
-    options.extensions = {}
-    for name, extension in pairs(extensions) do
-      options.extensions[name] = extensions.telescope_opts or {}
-    end
-    return options
-  end,
-  version = "*",
-  event = "VeryLazy",
-  dependencies = { "treesitter", "plenary", "devicons" },
-})
+table.insert(M,
+  {
+    "nvim-telescope/telescope.nvim",
+    name = "telescope",
+    opts = function()
+      local options = {}
+      local extensions = require("plugins.telescope.config").extensions
+      options.extensions = {}
+      for name, extension in pairs(extensions) do
+        options.extensions[name] = extensions.telescope_opts or {}
+      end
+      return options
+    end,
+    version = "*",
+    event = "VeryLazy",
+    dependencies = { "treesitter", "plenary", "devicons" },
+  }
+)
 
 return M
