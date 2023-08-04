0da3d7bf987ce72c730a29f8228cae97a6b1ea2d
Jade Humpherys <joshua.humpherys@protonmail.com>
2023-08-04 10:41:48 -0500

    reorganize illuminate

--- common/.config/nvim/lua/plugins/navigation/init.lua
+++ common/.config/nvim/lua/plugins/navigation/init.lua
@@ -2,14 +2,15 @@ return {
   {
     "RRethy/vim-illuminate",
     name = "illuminate",
+    init = function() vim.cmd[[hi def IlluminatedWordText gui=underline]] end,
     opts = {
       providers = { "lsp", "treesitter" },
       delay = 200,
       modes_allowlist = {'n'},
       min_count_to_highlight = 2,
     },
-    config = require("plugins.navigation.config").illuminate_config_function,
-    event = "VeryLazy",
+    config = function(_, opts) require("illuminate").configure(opts) end,
     dependencies = { "treesitter" },
+    event = "VeryLazy",
   },
 }
