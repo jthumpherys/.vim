7efc2e0d22693fdd7c34f30816c3e1b9466614dc
Jade Humpherys <joshua.humpherys@protonmail.com>
2023-09-13 10:03:17 -0500

    try to config, commented out sections are parts that are maybe not needed? testing needed

--- common/.config/nvim/lua/plugins/latex/init.lua
+++ common/.config/nvim/lua/plugins/latex/init.lua
@@ -3,19 +3,21 @@ return {
     "lervag/vimtex",
     config = function()
       vim.g.vimtex_view_method = "zathura"
-      vim.g.vimtex_compiler_method = "latexrun"
       vim.opt.conceallevel = 1
-      vim.g.tex_conceal = "abdmg"
-      vim.g:vimtex_toc_config = {
-        name = 'TOC',
-        layers = {'content', 'todo', 'include'},
-        resize = 1,
-        split_width = 50,
-        todo_sorted = 0,
-        show_help = 1,
-        show_numbers = 1,
-        mode = 2,
-      }
+      -- vim.g.tex_conceal = "abdmg"
+      vim.g.vimtex_mappings_enabled = 0
+      -- vim.g.vimtex_fold_enabled = 1
+      -- vim.g.vimtex_toc_config = {
+      --   name = 'TOC',
+      --   layers = {'content', 'todo', 'include'},
+      --   resize = 1,
+      --   split_width = 50,
+      --   todo_sorted = 0,
+      --   show_help = 1,
+      --   show_numbers = 1,
+      --   mode = 2,
+      -- }
     end,
+    lazy = false,
   },
 }
