215e4de774d3cf24baeac6efa27b47598881b27e
Jade Humpherys <joshua.humpherys@protonmail.com>
2023-09-13 10:04:33 -0500

    don't interfere with vimtex syntax highlighting (vimtex functionality is broken otherwise

--- common/.config/nvim/lua/plugins/treesitter.lua
+++ common/.config/nvim/lua/plugins/treesitter.lua
@@ -42,6 +42,7 @@ return {
       auto_install = false,
       highlight = {
         enable = true,
+        disable = { "latex" },
         additional_vim_regex_highlighting = { "markdown" },
       },
       indent = { enable = true },
