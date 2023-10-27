31f34053af360d74b073948817cd15c29ab6679f
Jade Humpherys <joshua.humpherys@protonmail.com>
2023-10-27 00:28:08 -0500

    switch to duskfox and adjust some colors

--- common/.config/nvim/lua/plugins/ui/config.lua
+++ common/.config/nvim/lua/plugins/ui/config.lua
@@ -1,23 +1,42 @@
--- palettes = {
--- 	carbonfox = {
--- 	},
--- 	duskfox = {
--- 		bg1 = "#0b0b0b",
--- 		bg0 = "#1d1d2b", -- Alt backgrounds (floats, statusline, ...)
--- 		bg3 = "#121820", -- 55% darkened from stock
--- 		sel0 = "#131b24",
--- 	},
--- },
--- specs = {
--- 	duskfox = {
--- 		inactive = "#101010",
--- 	},
--- 	carbonfox = {
--- 		inactive = "#090909", -- wash out colors instead?
--- 	},
--- },
--- groups = {
--- 	carbonfox = {
--- 		NormalNC = {}, -- fix carbonfox Non-current window
--- 	},
--- },
+local M = {}
+
+function M.nightfox(_, options)
+  local C = require("nightfox.lib.color")
+  local Shade = require("nightfox.lib.shade")
+  local bg = C.from_hex("#1b1029")
+  local opts = { options = options }
+  opts.palettes = {
+    duskfox = {
+      bg0 = bg:shade(-0.2):to_hex(),
+      bg1 = bg:to_hex(),
+      bg2 = bg:shade(0.07):to_hex(),
+      bg3 = bg:shade(0.15):to_hex(),
+      bg4 = bg:shade(0.3):to_hex(),
+
+      -- white = Shade.new("")
+
+      purple = Shade.new("#b08ddf", "#c9afe5", "#9570c2"),
+      magenta = Shade.new("#cc58d5", "#e777ee", "#b437c0"),
+      blue = Shade.new("#567fba", "#8aa5dd", "#445a8c"),
+    },
+  }
+  opts.palettes.duskfox.white = opts.palettes.duskfox.purple
+  opts.specs = {
+    duskfox = {
+      syntax = {
+        -- builtin0 = "magenta.bright",
+        -- keyword = "purple",
+        variable = "purple",
+        -- field = "green",
+        -- field = "magenta.bright",
+        -- func = "pink",
+        -- string = "pink.bright",
+        -- bracket = "pink.dim",
+      },
+    },
+  }
+  require("nightfox").setup(opts)
+  vim.cmd[[colorscheme duskfox]]
+end
+
+return M
--- common/.config/nvim/lua/plugins/ui/init.lua
+++ common/.config/nvim/lua/plugins/ui/init.lua
@@ -1,135 +1,128 @@
+local config = require("plugins.ui.config")
+
 return {
   {
-    "folke/tokyonight.nvim",
+    "Shatur/neovim-ayu",
+    name = "ayu",
+    enabled = false,
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    enabled = false,
+    opts = {options ={ theme = "duskfox" }},
+    lazy = false,
+    priority = 500,
+    config = true,
+  },
+
+  {
+    "EdenEast/nightfox.nvim",
+    name = "nightfox",
     lazy = false,
     priority = 1000,
+    version = "*",
     opts = {
-      style = "night",
-      styles = {
-	keywords = { italic = false },
+      transparent = false,
+      dim_inactive = true,
+      inverse = {
+        match_paren = true,
       },
-      lualine_bold = true,
+      styles = {
+        comments = "italic",
+        functions = "bold",
+      },
     },
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd[[colorscheme tokyonight]]
-    end
+    config = config.nightfox,
+    build = ":NightfoxCompile",
+    dependencies = { "treesitter", "ts-playground" },
   },
-	{
-		"Shatur/neovim-ayu",
-		name = "ayu",
-	},
 
-	{
-		"nvim-lualine/lualine.nvim",
-		opts = { theme = "tokyonight" },
-    lazy = false,
+  {
+    "freddiehaddad/feline.nvim",
+    -- enabled = false,
+    name = "feline",
+    config = function()
+      require("plugins.ui.feline")
+    end,
+    -- dependencies = "devicons",
+    event = { "BufReadPre", "BufNewFile" },
+  },
+
+  {
+    "stevearc/dressing.nvim",
     config = true,
-	},
+    event = "VeryLazy",
+  },
 
-	{
-		"EdenEast/nightfox.nvim",
-		enabled = false,
-		name = "nightfox",
-		lazy = false,
-		priority = 100,
-		version = "*",
-		opts = {
-			options = {
-				transparent = false,
-				dim_inactive = true,
-			},
-		},
-		config = function(_, opts)
-			require("nightfox").setup(opts)
-			vim.cmd("colorscheme carbonfox")
-		end,
-		build = ":NightfoxCompile",
-		dependencies = { "treesitter", "ts-playground" },
-	},
+  {
+    "sitiom/nvim-numbertoggle",
+    name = "numbertoggle",
+    init = function()
+      vim.opt.number = true
+    end,
+    event = { "BufReadPost", "BufNewFile" },
+  },
 
-	{
-		"freddiehaddad/feline.nvim",
-		enabled = false,
-		name = "feline",
-		config = function()
-			require("plugins.ui.feline")
-		end,
-		dependencies = "devicons",
-		event = { "BufReadPre", "BufNewFile" },
-	},
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    name = "indent",
+    main = "ibl",
+    version = "*",
+    opts = {
+      indent = {
+        char = "▏",
+      },
+      scope = {
+        char = "▎",
+        show_start = false,
+        show_end = false,
+        highlight = "Label",
+      },
+    },
+    config = true,
+    event = "VeryLazy",
+  },
 
-	{
-		"stevearc/dressing.nvim",
-		config = true,
-		event = "VeryLazy",
-	},
+  -- this don't work
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+      local delim = require("rainbow-delimiters")
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [""] = delim.strategy["global"],
+          latex = delim.strategy["local"],
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+          lua = "rainbow-blocks",
+          latex = "rainbow-blocks",
+        },
+        highlight = {
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterGreen",
+      },
+    }
+    end,
+    event = "VeryLazy",
+    dependencies = { "treesitter" },
+  },
 
-	{
-		"sitiom/nvim-numbertoggle",
-		name = "numbertoggle",
-		init = function()
-			vim.opt.number = true
-		end,
-		event = { "BufReadPost", "BufNewFile" },
-	},
-
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		name = "indent",
-		main = "ibl",
-		version = "*",
-		opts = {
-			indent = {
-				char = "▏",
-			},
-			scope = {
-				char = "▎",
-				show_start = false,
-				show_end = false,
-				highlight = "Label",
-			},
-		},
-		config = true,
-		event = "VeryLazy",
-	},
-
-	-- this don't work
-	{
-		"HiPhish/rainbow-delimiters.nvim",
-		config = function()
-			local delim = require("rainbow-delimiters")
-			vim.g.rainbow_delimiters = {
-				strategy = {
-					[""] = delim.strategy["global"],
-					latex = delim.strategy["local"],
-				},
-				query = {
-					[""] = "rainbow-delimiters",
-					lua = "rainbow-blocks",
-					latex = "rainbow-blocks",
-				},
-				highlight = {
-					"RainbowDelimiterViolet",
-					"RainbowDelimiterCyan",
-					"RainbowDelimiterBlue",
-					"RainbowDelimiterGreen",
-				},
-			}
-		end,
-		event = "VeryLazy",
-		dependencies = { "treesitter" },
-	},
   {
     "NvChad/nvim-colorizer.lua",
     main = "colorizer",
     config = true,
     opts = {
       user_default_options = {
-	RRGGBBAA = true,
-	AARRGGBB = true,
-	always_update = true,
+        names = false,
+        RRGGBBAA = true,
+        AARRGGBB = true,
+        always_update = true,
       },
+      mode = "background",
     },
   },
 }
