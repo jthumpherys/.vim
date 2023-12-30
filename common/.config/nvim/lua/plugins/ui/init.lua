6063855cf11d560aedc1648ac71201f9fe4be04b
Jade Humpherys <jade.topaz@proton.me>
2023-12-29 22:38:45 -0600

    remove debugging log, oops

--- common/.config/nvim/lua/plugins/ui/init.lua
+++ common/.config/nvim/lua/plugins/ui/init.lua
@@ -3,7 +3,6 @@ return {
     "sitiom/nvim-numbertoggle",
     name = "numbertoggle",
     init = function()
-      require("utils").log("here")
       vim.opt.number = true
     end,
     event = { "BufReadPost", "BufNewFile" },
