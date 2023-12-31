0e086de04421f7987f59abd06f0eb208915cb57b
Jade Humpherys <jade.topaz@proton.me>
2023-12-30 20:34:52 -0600

    add `indent-blankline.nvim`

--- common/.config/nvim/lua/plugins/ui/init.lua
+++ common/.config/nvim/lua/plugins/ui/init.lua
@@ -4,4 +4,25 @@ return {
     name = "numbertoggle",
     event = { "BufReadPost", "BufNewFile" },
   },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    name = "indent",
+    main = "ibl",
+    opts = {
+      indent = {
+        char = "▏",
+      },
+      scope = {
+        char = "▎",
+        show_start = false,
+        show_end = false,
+        highlight = "Label",
+        exclude = { language = { "toml" }, },
+      },
+    },
+    config = true,
+    version = "*",
+    event = { "BufReadPost", "BufNewFile" },
+  },
 }
