4c8539c98b98b25a9396c241a7fb7920ad194413
Jade Humpherys <jade.topaz@proton.me>
2024-02-04 02:53:56 -0600

    move colorscheme to color.lua

--- common/.config/nvim/lua/plugins/color.lua
+++ common/.config/nvim/lua/plugins/color.lua
@@ -5,6 +5,14 @@ return {
     priority = 1000,
     opts = {
       style = "night",
+      on_highlights = function(hl, c)
+        -- hl.CursorLineNr = {
+        --   bg = c.bg_highlight,
+        -- }
+        hl.CursorLineSign = {
+          bg = c.bg_highlight,
+        }
+      end,
     },
     config = function(_, opts)
       require("tokyonight").setup(opts)
