c32d4639b0d390dfd00b4f50ee60dd6baa240311
Jade Humpherys <jade.topaz@proton.me>
2024-02-07 13:22:50 -0600

    fix: correct typo

--- common/.config/nvim/lua/plugins/ui/init.lua
+++ common/.config/nvim/lua/plugins/ui/init.lua
@@ -43,7 +43,7 @@ return {
         exclude = { language = { "toml", "gitconfig" }, },
       },
     },
-    confic = true,
+    config = true,
     version = "*",
     event = { "BufReadPost", "BufNewFile" },
   },
