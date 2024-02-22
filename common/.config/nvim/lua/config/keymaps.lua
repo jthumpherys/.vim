86d5a86b48c26288b56b684f0ec59c19b56fad06
Jade Humpherys <jade.topaz@proton.me>
2024-02-21 21:42:51 -0600

    feat(nvim): add mapping `<leader>o` to insert new line at end of file

--- common/.config/nvim/lua/config/keymaps.lua
+++ common/.config/nvim/lua/config/keymaps.lua
@@ -5,6 +5,7 @@ function M.setup()
   vim.keymap.set('n', 's', '<c-w>', {desc = "Switch split windows rapidly"})
   vim.keymap.set('n', 'g-', '<c-o>', {desc = "Return to previous position"})
   vim.keymap.set('n', 'g_', '<c-i>', {desc = "Jump forward positions"})
+  vim.keymap.set('n', '<leader>o', 'Go', {desc = "Insert at new line EOF"})
 end
 
 return M
