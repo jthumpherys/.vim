81c8afd05bd9753f651b57f108808f3f52de0d78
Jade Humpherys <jade.topaz@proton.me>
2024-02-26 14:31:36 -0600

    feat(nvim): add `nvim-ufo` plugin

--- common/.config/nvim/lua/plugins/ui.lua
+++ common/.config/nvim/lua/plugins/ui.lua
@@ -75,4 +75,44 @@ return {
     config = true,
     event = "VeryLazy",
   },
+
+  {
+    "kevinhwang91/nvim-ufo",
+    opts = {
+      provider_selector = function(bufnr, _, _)
+        if vim.lsp.get_clients({bufnr=bufnr}) then
+          return { "lsp", "indent" }
+        else
+          return { "treesitter", "indent" }
+        end
+      end,
+    },
+    config = function(opts)
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+      require("ufo").setup(opts)
+    end,
+    init = function()
+      require("plugins.lsp.language_servers").extend_capabilities(
+        function()
+          return {
+            textDocument = {
+              foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true,
+              }
+            }
+          }
+        end
+      )
+      table.insert(require("plugins.whichkey.utils").to_register, {
+        zR = { function() require("ufo").openAllFolds() end, "Open all folds" },
+        zM = { function() require("ufo").openAllFolds() end, "Close all folds" },
+        zr = { function() require("ufo").openFoldsExceptKinds() end, "Decrease fold level" },
+        zm = { function() require("ufo").closeFoldsWith() end, "Increase fold level" },
+      })
+    end,
+    dependencies = { "kevinhwang91/promise-async" },
+    keys = { 'zc', 'zo', 'zC', 'zO' },
+  },
 }
