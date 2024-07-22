ebd14d2fbc1e6667db7e130e3a69c9b48b821a48
Jade Humpherys <jade.topaz@proton.me>
2024-07-21 20:51:18 -0500

    fix  keymaps with improper functions

--- common/.config/nvim/lua/plugins/editing/keymaps.lua
+++ common/.config/nvim/lua/plugins/editing/keymaps.lua
@@ -6,26 +6,26 @@ M.comment = {
     function()
       return require("Comment.api").call("toggle.linewise", 'g@')()
     end,
-    group = "Comment",
+    group = "comment",
     expr = true,
   },
   { '<leader>cc', function() require("Comment.api").toggle.linewise.current() end, desc = "Current Line" },
-  { '<leader>cO', function() require("Comment.api").toggle.linewise.above() end, desc = "Insert Previous Line" },
-  { '<leader>co', function() require("Comment.api").toggle.linewise.below() end, desc = "Insert Next Line" },
-  { '<leader>cA', function() require("Comment.api").locked("insert.linewise.eol") end, desc = "Insert End of Line" },
+  { '<leader>cO', function() require("Comment.api").insert.linewise.above() end, desc = "Insert Previous Line" },
+  { '<leader>co', function() require("Comment.api").insert.linewise.below() end, desc = "Insert Next Line" },
+  { '<leader>cA', function() require("Comment.api").locked("insert.linewise.eol")() end, desc = "Insert End of Line" },
 
   {
     '<leader>b',
     function()
       return require("Comment.api").call("toggle.blockwise", 'g@')()
     end,
-    group = "Block Comment",
+    group = "comment block",
     expr = true,
   },
   { '<leader>bc', function() require("Comment.api").toggle.blockwise.current() end, desc = "Current Line" },
-  { '<leader>bO', function() require("Comment.api").toggle.blockwise.above() end, desc = "Insert Previous Line" },
-  { '<leader>bo', function() require("Comment.api").toggle.blockwise.below() end, desc = "Insert Next Line" },
-  { '<leader>bA', function() require("Comment.api").locked("insert.blockwise.eol") end, desc = "Insert End of Line" },
+  { '<leader>bO', function() require("Comment.api").insert.blockwise.above() end, desc = "Insert Previous Line" },
+  { '<leader>bo', function() require("Comment.api").insert.blockwise.below() end, desc = "Insert Next Line" },
+  { '<leader>bA', function() require("Comment.api").locked("insert.blockwise.eol")() end, desc = "Insert End of Line" },
 }
 
 return M
